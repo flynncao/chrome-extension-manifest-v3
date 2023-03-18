69078d5588b0227c1bd3f2392d0a6ee67eaaaf37
Flynn <ppstream123@126.com>
2023-03-18 22:19:33 +0800

    chore: separate dev and prod webpack configs

--- dist/background.js
+++ dist/background.js
@@ -1,16 +1,2 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**************************************!*\
-  !*** ./src/background/background.ts ***!
-  \**************************************/
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("I have been installed");
-});
-
-chrome.bookmarks.onCreated.addListener(() => {
-  console.log("I just bookmarked an page.");
-});
-
-/******/ })()
-;
+chrome.runtime.onInstalled.addListener((()=>{console.log("I have been installed")})),chrome.bookmarks.onCreated.addListener((()=>{console.log("I just bookmarked an page.")}));
 //# sourceMappingURL=background.js.map
